2828b0807b69957cc6ed069a1d4234c9de459a1b
VincentCelestial <vincentcelestial@icloud.comgit config --global user.email vincentcelestial@icloud.comgit config --global user.email vincentcelestial@icloud.com>
2023-08-17 21:25:15 -0500

    cinco

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -10,7 +10,12 @@ arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
-// Your code here
+const arrowMirrorArray = (array) => {
+  let result = []
+
+  result.push(...array, ...array.reverse)
+  return result
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
