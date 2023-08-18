eeabe93dd397e349e8a51a4cb83f6c09104437f5
Yasuhiro Sekiyama <sekiyamayasuhiro@gmail.com>
2023-08-17 21:19:16 -0700

    problem onze

--- problems/11-lazy-adder.js
+++ problems/11-lazy-adder.js
@@ -22,7 +22,15 @@ AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
   fat-arrow function
 ***********************************************************************/
 
-// Your code here
+// function lazyAdder (firstNum) {
+//   return function(secondNum) {
+//     return function(thirdNum) {
+//       return firstNum + secondNum + thirdNum;
+//     }
+//   }
+// }
+
+const lazyAdder = (firstNum) => (secondNum) => (thirdNum) => firstNum + secondNum + thirdNum
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
