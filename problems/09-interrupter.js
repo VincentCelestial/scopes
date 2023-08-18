8e56d413c0ec3ee42724323b7a56d25a0700c7a7
Wei Cheng Chen <DrChenWeiCheng@gmail.com>
2023-08-17 20:28:40 -0700

    numero nueve

--- problems/09-interrupter.js
+++ problems/09-interrupter.js
@@ -17,6 +17,39 @@ console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 ***********************************************************************/
 
 // Your code here
+// function interrupter(interruptingWord) {
+//   return function(element) {
+//     let words = element.split(' ');
+//     let result = []
+//     for (let i = 0 ; i < words.length ; i++) {
+//       result.push(words[i]);
+//       if (i >= 0 && i < words.length - 1) {
+//         result.push(interruptingWord)
+//       }
+//     }
+//     return result.join(' ')
+//   }
+// }
+function interrupter(interruptingWord) {
+  return function(element) {
+    let words = element.split(' ');
+    let interruptedSentence = '';
+    words.forEach((word, index) => {
+      interruptedSentence += word;
+      if (index >= 0 && index < words.length -1) {
+        interruptedSentence += ' ' + interruptingWord + ' ';
+      }
+    })
+    return interruptedSentence
+  }
+}
+let rudePerson = interrupter("what"); // => returns a function
+console.log(rudePerson("how are you")); // prints "how what are what you"
+console.log(rudePerson("I like pie")); // prints "I what like what pie"
+
+
+let rudePerson2 = interrupter("yo"); // => returns a function
+console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
